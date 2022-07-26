7341b6d48f7e232db4bd1948dfe86397ec1a3c78
AnkushLambdatest <107558074+AnkushLambdatest@users.noreply.github.com>
2022-07-26 09:47:46 +0530

    Update stringifySpec.js

--- test/stringifySpec.js
+++ test/stringifySpec.js
@@ -1,7 +1,7 @@
 'use strict';
 
 describe('toDebugString', function() {
-  it.skip('should convert its argument to a string', function() {
+  it('should convert its argument to a string', function() {
     expect(toDebugString('string')).toEqual('string');
     expect(toDebugString(123)).toEqual('123');
     expect(toDebugString({a:{b:'c'}})).toEqual('{"a":{"b":"c"}}');
